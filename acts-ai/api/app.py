926a11a8c55b35a3fc85ad1a8cbc2c39ca54f3bf
devanshu <devanshusenp@gmail.com>
2024-09-14 15:15:23 -0400

    added intent detector, general skeleton for function calls

--- acts-ai/api/app.py
+++ acts-ai/api/app.py
@@ -3,28 +3,54 @@ Main web application service. Serves the frontend as well as
 API routes for [functionality].
 """
 
+import json
+
 import modal
-from tune import chat_completion
+
+from api.models import DetectedIntent, Intent
+from api.prompts import INTENT_DETECTOR
+from api.tune import chat_completion
 
 app = modal.App("example-hello-world")
 
 
-@app.function()
-def f(query):
-    system_content = (
-        "You answer all questions with only emojis. You are a helpful assistant."
-    )
-    stream = False
+@app.function(secrets=[modal.Secret.from_name("hackmit")])
+def intent(context: str) -> DetectedIntent:
+    """Detects which predefined intent context falls into."""
     res = chat_completion(
-        system_context=system_content, user_question=query, stream=stream
+        system_context=INTENT_DETECTOR, user_question=context, stream=False
     )
-    return res
+    detected_intent = res[0].choices[0].message.content
+    formatted_detected_intent = json.loads(detected_intent)
+    typed_detected_intent = DetectedIntent(**formatted_detected_intent)
+    return typed_detected_intent
+
+
+@app.function(secrets=[modal.Secret.from_name("hackmit")])
+def respond_to_chat(context: str) -> str:
+    """Responds to a chat message."""
+    detected_intent = intent.local(context=context)
+
+    match detected_intent.intent:
+        case Intent.CONVERSATION:
+            # build a respond to chat function.
+            return "I'm here to help. What's on your mind?"
+        case Intent.EMERGENCY:
+            # build a call emergency function.
+            return "I'm sorry. I can't do that."
+        case Intent.THERAPIST_REQUEST:
+            # build a therapist request function.
+            return "Here's a list of therapists."
+
+    return "NONE"
 
 
 @app.local_entrypoint()
 def main():
     # run the function locally
-    print(f.local("whats the weather like today?"))
+    print(respond_to_chat.local("i want to kill myself"))
+    print(respond_to_chat.local("what can you do?"))
+    print(respond_to_chat.local("find me a therapist?"))
 
     # run the function remotely on Modal
-    print(f.remote("whats the weather like today?"))
+    # print(respond_to_chat.remote("whats the weather like today?"))
